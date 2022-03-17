ef8cdb601c2bb9b892ef502729dd74b2dca50446
JM Mac <johnmarkmancol@gmail.com>
2022-03-17 11:51:46 +0800

    static values assignment via configuration

--- src/AllPayment.php
+++ src/AllPayment.php
@@ -4,10 +4,16 @@ namespace HumanIncubator\AllPaymentHelper;
 
 use HumanIncubator\AllPaymentHelper\Invoice;
 use HumanIncubator\AllPaymentHelper\Payment;
+use Illuminate\Support\Facades\Config;
 
 Class AllPayment {
-    static $all_payment_url = 'http://127.0.0.1:8000';
-    static $client_api_key = 'testkey123';
+    static $all_payment_url;
+    static $client_api_key;
+
+    public function __construct() {
+        self::$all_payment_url = config('services.allpayment.api_url');
+        self::$client_api_key = config('services.allpayment.token');
+    }
 
     public static function create_invoice($amount, $callback_url = '', $order_id = '') {
 
